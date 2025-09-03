6c9ec183fdfcd420bdab394bf8f25c02664b2460
Ruukshan <rukshaninduwara123@gmail.com>
2025-09-04 00:32:56 +0530

    Get a reference from vuetify and create a FormPage.vue file

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
-<script setup>
-
+<script>
+import FormPage from './components/FormPage.vue';
+export default {
+  components: { FormPage },
+};
 </script>
 
 <template>
-  <div> 
-  </div>
+  <v-app>
+    <FormPage />
+  </v-app> 
+  
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
