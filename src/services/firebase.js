3482cab2b7f66bbc898274bd536093e98b957d43
Ruukshan <rukshaninduwara123@gmail.com>
2025-09-03 23:55:32 +0530

    Initialize Firebase and set config service/firebase.js

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0xLWy1oL5lVD6Jtbct-43E-JjFIthMwQ",
+  authDomain: "vue-form-app-13761.firebaseapp.com",
+  projectId: "vue-form-app-13761",
+  storageBucket: "vue-form-app-13761.firebasestorage.app",
+  messagingSenderId: "934703435134",
+  appId: "1:934703435134:web:16c65867a338ee77b5772f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
