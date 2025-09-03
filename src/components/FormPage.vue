3d88103247244b42cd955efcf28d815e71ff6a0d
Ruukshan <rukshaninduwara123@gmail.com>
2025-09-04 01:12:08 +0530

    Setting up Name , E-mai and Select options with validations.

--- src/components/FormPage.vue
+++ src/components/FormPage.vue
@@ -1,99 +1,89 @@
 <template>
-  <form @submit.prevent="submit">
-    <v-text-field
-      v-model="name.value.value"
-      :counter="10"
-      :error-messages="name.errorMessage.value"
-      label="Name"
-    ></v-text-field>
-
-    <v-text-field
-      v-model="phone.value.value"
-      :counter="7"
-      :error-messages="phone.errorMessage.value"
-      label="Phone Number"
-    ></v-text-field>
-
-    <v-text-field
-      v-model="email.value.value"
-      :error-messages="email.errorMessage.value"
-      label="E-mail"
-    ></v-text-field>
-
-    <v-select
-      v-model="select.value.value"
-      :error-messages="select.errorMessage.value"
-      :items="items"
-      label="Select"
-    ></v-select>
-
-    <v-checkbox
-      v-model="checkbox.value.value"
-      :error-messages="checkbox.errorMessage.value"
-      label="Option"
-      type="checkbox"
-      value="1"
-    ></v-checkbox>
-
-    <v-btn
-      class="me-4"
-      type="submit"
-    >
-      submit
-    </v-btn>
-
-    <v-btn @click="handleReset">
-      clear
-    </v-btn>
-  </form>
+<h1>Registration Form</h1>
+  <v-form v-model="valid">
+    <v-container>
+          <v-text-field
+            v-model="name"
+            :counter="10"
+            :rules="nameRules"
+            label="Name"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+          <v-select
+            v-model="select"
+            :items="items"
+            :rules="selectRules"
+            label="Select"
+            required
+          ></v-select>
+          <v-btn
+            :disabled="!valid"
+            color="success"
+            class="mr-4"
+          >
+            Submit
+          </v-btn>
+    </v-container>
+  </v-form>
 </template>
-<script setup>
-  import { ref } from 'vue'
-  import { useField, useForm } from 'vee-validate'
+<script>
+  export default {
+    data: () => ({
+      valid: false,
+      name: '',
+      nameRules: [
+        value => {
+          if (value) return true
 
-  const { handleSubmit, handleReset } = useForm({
-    validationSchema: {
-      name (value) {
-        if (value?.length >= 2) return true
+          return 'Name is required.'
+        },
+        value => {
+          if (value?.length <= 10) return true
 
-        return 'Name needs to be at least 2 characters.'
-      },
-      phone (value) {
-        if (/^[0-9-]{7,}$/.test(value)) return true
+          return 'Name must be less than 10 characters.'
+        },
+      ],
+      email: '',
+      emailRules: [
+        value => {
+          if (value) return true
 
-        return 'Phone number needs to be at least 7 digits.'
-      },
-      email (value) {
-        if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
+          return 'E-mail is required.'
+        },
+        value => {
+          if (/.+@.+\..+/.test(value)) return true
 
-        return 'Must be a valid e-mail.'
-      },
-      select (value) {
-        if (value) return true
+          return 'E-mail must be valid.'
+        },
+      ],
+      select: null,
+      items: [
+        'Developer',
+        'Designer',
+        'Tester',
+        'project Manager',
+      ],
+      selectRules: [
+        value => {
+          if (value) return true
 
-        return 'Select an item.'
-      },
-      checkbox (value) {
-        if (value === '1') return true
-
-        return 'Must be checked.'
-      },
-    },
-  })
-  const name = useField('name')
-  const phone = useField('phone')
-  const email = useField('email')
-  const select = useField('select')
-  const checkbox = useField('checkbox')
-
-  const items = ref([
-    'Item 1',
-    'Item 2',
-    'Item 3',
-    'Item 4',
-  ])
-
-  const submit = handleSubmit(values => {
-    alert(JSON.stringify(values, null, 2))
-  })
+          return 'Select is required.'
+        }
+      ],
+    }),
+  }
 </script>
+<style scoped>
+h1 {
+  text-align: center;
+  margin-top: 50px;
+  font-size: 40px;
+  color: brown;
+}
+</style>
