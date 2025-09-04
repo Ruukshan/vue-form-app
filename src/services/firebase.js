663c0e5bec1e1cb12c339ee0172eab968d91a1ef
Ruukshan <rukshaninduwara123@gmail.com>
2025-09-04 18:01:24 +0530

    Setting up .env for security

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB0xLWy1oL5lVD6Jtbct-43E-JjFIthMwQ",
-  authDomain: "vue-form-app-13761.firebaseapp.com",
-  projectId: "vue-form-app-13761",
-  storageBucket: "vue-form-app-13761.firebasestorage.app",
-  messagingSenderId: "934703435134",
-  appId: "1:934703435134:web:16c65867a338ee77b5772f"
+  apiKey: import.meta.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VUE_APP_FIREBASE_APP_ID,
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
